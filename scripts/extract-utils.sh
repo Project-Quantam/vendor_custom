b4a1acaa10f8aae438ce065aca8bda179c9d4732
Rashed Abdel-Tawab <rashed@linux.com>
2017-04-04 02:48:18 -0400

    extract_files: Add support for specifying blob sections.
    Sometimes the need arises to update one specific subset of blobs
    while holding others steady. Introduce a way to specify the "section"
    of blobs to update (obviously requires the proprietary-files to be
    properly split up first)
    
    Requires additional changes in device tree extract_files.sh to support
    passing the additional arguments
    
    Change-Id: I98feab56f8bfade2818b1b5264e019440d4c57c5

--- scripts/extract-utils.sh
+++ scripts/extract-utils.sh
@@ -538,6 +538,7 @@ function _adb_connected {
 # parse_file_list:
 #
 # $1: input file
+# $2: blob section in file - optional
 #
 # Sets PRODUCT_PACKAGES and PRODUCT_COPY_FILES while parsing the input file
 #
@@ -549,6 +550,14 @@ function parse_file_list() {
         echo "Input file "$1" does not exist!"
         exit 1
     fi
+
+    if [ $# -eq 2 ]; then
+        LIST=$TMPDIR/files.txt
+        cat $1 | sed -n '/# '"$2"'/I,/^\s*$/p' > $LIST
+    else
+        LIST=$1
+    fi
+
 
     PRODUCT_PACKAGES_LIST=()
     PRODUCT_PACKAGES_HASHES=()
@@ -578,7 +587,7 @@ function parse_file_list() {
             PRODUCT_COPY_FILES_HASHES+=("$HASH")
         fi
 
-    done < <(egrep -v '(^#|^[[:space:]]*$)' "$1" | LC_ALL=C sort | uniq)
+    done < <(egrep -v '(^#|^[[:space:]]*$)' "$LIST" | LC_ALL=C sort | uniq)
 }
 
 #
@@ -762,6 +771,7 @@ function fix_xml() {
 #
 # $1: file containing the list of items to extract
 # $2: path to extracted system folder, an ota zip file, or "adb" to extract from device
+# $3: section in list file to extract - optional
 #
 function extract() {
     if [ -z "$OUTDIR" ]; then
@@ -769,7 +779,7 @@ function extract() {
         exit 1
     fi
 
-    parse_file_list "$1"
+    parse_file_list "$1" "$3"
 
     # Allow failing, so we can try $DEST and/or $FILE
     set +e
