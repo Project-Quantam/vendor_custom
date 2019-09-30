457cb2045fc1ca2f23c6add837e4dfe1ce5de010
LuK1337 <priv.luk@gmail.com>
2019-09-30 17:55:58 +0200

    kernel: Switch back to host openssl entirely
    * OpenSSL cannot be statically linked because of
      getaddrinfo and gethostbyname calls.
    * Also according to soong it's whitelisted:
      https://github.com/LineageOS/android_build_soong/blob/33f95299622a2c6c332cd428a4f324771f5b9575/ui/build/paths/config.go#L98
    
    Change-Id: I1718bd98e7ac30d31de38c2cd3bc3e51b7100ce4

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -112,15 +112,7 @@ ifeq ($(KERNEL_ARCH),arm64)
   KERNEL_MAKE_FLAGS += CFLAGS_MODULE="-fno-pic"
 endif
 
-ifeq ($(HOST_OS),darwin)
-  KERNEL_MAKE_FLAGS += C_INCLUDE_PATH=$(BUILD_TOP)/external/elfutils/libelf:/usr/local/opt/openssl/include
-  KERNEL_MAKE_FLAGS += LIBRARY_PATH=/usr/local/opt/openssl/lib
-else
-  KERNEL_MAKE_FLAGS += C_INCLUDE_PATH=$(BUILD_TOP)/prebuilts/openssl/$(HOST_OS)-x86/1.1.1/include
-  KERNEL_MAKE_FLAGS += LIBRARY_PATH=$(BUILD_TOP)/prebuilts/openssl/$(HOST_OS)-x86/1.1.1/lib/x86_64-linux-gnu
-  KERNEL_MAKE_FLAGS += HOSTCFLAGS="-L $(BUILD_TOP)/prebuilts/openssl/$(HOST_OS)-x86/1.1.1/lib/x86_64-linux-gnu"
-  KERNEL_MAKE_FLAGS += HOSTLDFLAGS="-L $(BUILD_TOP)/prebuilts/openssl/$(HOST_OS)-x86/1.1.1/lib/x86_64-linux-gnu"
-endif
+KERNEL_MAKE_FLAGS += HOSTCFLAGS="-I/usr/include -I/usr/include/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu -L/usr/lib64"
 
 TOOLS_PATH_OVERRIDE := LD_LIBRARY_PATH=$(BUILD_TOP)/prebuilts/openssl/$(HOST_OS)-x86/1.1.1/lib:$$LD_LIBRARY_PATH
 
@@ -128,7 +120,7 @@ ifneq ($(TARGET_KERNEL_ADDITIONAL_FLAGS),)
   KERNEL_MAKE_FLAGS += $(TARGET_KERNEL_ADDITIONAL_FLAGS)
 endif
 
-TOOLS_PATH_OVERRIDE += \
+TOOLS_PATH_OVERRIDE := \
     PATH=$(BUILD_TOP)/prebuilts/custom-tools/$(HOST_OS)-x86/bin:$$PATH \
     LD_LIBRARY_PATH=$(BUILD_TOP)/prebuilts/custom-tools/$(HOST_OS)-x86/lib:$$LD_LIBRARY_PATH \
     PERL5LIB=$(BUILD_TOP)/prebuilts/custom-tools/$(HOST_OS)-x86/lib/perl5
