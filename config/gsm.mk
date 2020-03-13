f368c47879156b97bca702e54c28634322ecb223
Alexander Koskovich <zvnexus@outlook.com>
2020-03-12 22:26:08 -0400

    Use CarrierSettings for APNs from QQ3A.200705.002.
    Change-Id: I7529b247f6b6ca010197d94cb852b79101dcce04

--- config/gsm.mk
+++ config/gsm.mk
@@ -1,3 +1,10 @@
+# Carrier Settings.
+PRODUCT_PACKAGES += \
+    CarrierSettings
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/custom/prebuilt/product/etc/CarrierSettings,$(TARGET_COPY_OUT_PRODUCT)/etc/CarrierSettings)
+
 # SIM Toolkit.
 PRODUCT_PACKAGES += \
     CellBroadcastReciever \
