97027f15891547fa53dd43983798e70cab5cd98a
TheScarastic <warabhishek@gmail.com>
2020-05-25 10:09:14 +0000

    soong: Add flag for FOD extension.
    Change-Id: Ib45706cc50fa88b9b05af1c68eec9d1f08d70569

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -6,6 +6,10 @@ type Product_variables struct {
 	Target_shim_libs struct {
 		Cppflags []string
 	}
+	Target_surfaceflinger_fod_lib struct {
+		Cflags []string
+		Whole_static_libs []string
+	}
 	Target_uses_prebuilt_dynamic_partitions struct {
 		Cflags []string
 	}
@@ -14,5 +18,6 @@ type Product_variables struct {
 type ProductVariables struct {
 	Target_init_vendor_lib  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
+	Target_surfaceflinger_fod_lib  *string `json:",omitempty"`
 	Target_uses_prebuilt_dynamic_partitions  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -5,6 +5,7 @@ _json_contents := $(_json_contents)    "Custom":{$(newline)
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str_omitempty, Target_shim_libs, $(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
 $(call add_json_str_omitempty, Target_init_vendor_lib, $(TARGET_INIT_VENDOR_LIB))
+$(call add_json_str_omitempty, Target_surfaceflinger_fod_lib, $(TARGET_SURFACEFLINGER_FOD_LIB))
 $(call add_json_bool, Target_uses_prebuilt_dynamic_partitions,   $(filter true,$(TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
