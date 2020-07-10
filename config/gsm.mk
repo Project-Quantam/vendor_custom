717126f9d3bdc8eed0843f077015c7a139abc251
Alexander Koskovich <zvnexus@outlook.com>
2020-07-10 13:44:47 -0400

    Move QTI telephony products to vendor.
    Change-Id: I760c6a232195996c03f3180c866c013e406ac8b6

--- config/gsm.mk
+++ config/gsm.mk
@@ -9,3 +9,16 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     CellBroadcastReciever \
     Stk
+
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
