d1cfa24bdbcc02509891910f93eb3861e436bfb5
Alexander Koskovich <zvnexus@outlook.com>
2020-07-10 13:04:23 -0400

    Inherit from our GMS repository.
    Change-Id: I309e6f3ae1b4171eaa3e61da8e85dda9414ffe8a

--- config/common.mk
+++ config/common.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# GMS
+$(call inherit-product, vendor/google/gms/gms-vendor.mk)
+
 # GSM
 $(call inherit-product, vendor/custom/config/gsm.mk)
 
